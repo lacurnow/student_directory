dcce668558cdf5446054686371af1e439ca5e259
Lucinda Curnow <lucinda.curnow@gmail.com>
2022-03-01 16:36:09 +0000

    Added comments

--- student_directory.rb
+++ student_directory.rb
@@ -11,6 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+#Print total number of students
 print "Overall, we have "
 print 11
 puts " great students"
